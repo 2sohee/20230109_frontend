48aa076b88c9ab1253c07d8905f79570690f9e6d
2sohee <thgml9847@gmail.com>
2023-02-17 17:55:47 +0900

    firebase

---
+++ learn_firebase/src/firebase/index.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4MljFqy_YuqsmoCmIApClaPuL99W3_fo",
+  authDomain: "my-first-project-9980f.firebaseapp.com",
+  projectId: "my-first-project-9980f",
+  storageBucket: "my-first-project-9980f.appspot.com",
+  messagingSenderId: "712434839292",
+  appId: "1:712434839292:web:8655b014c10552d096f179",
+  measurementId: "G-ZWF5EPX9T9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
